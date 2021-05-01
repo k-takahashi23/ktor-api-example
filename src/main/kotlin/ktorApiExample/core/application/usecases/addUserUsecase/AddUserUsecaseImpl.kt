50b829703e8865fd170be2b3365bfb856ea215f4
k-takahashi23 <efl.sad23@gmail.com>
2021-05-02 01:29:14 +0900

    MOD: use UUID

--- src/main/kotlin/ktorApiExample/core/application/usecases/addUserUsecase/AddUserUsecaseImpl.kt
+++ src/main/kotlin/ktorApiExample/core/application/usecases/addUserUsecase/AddUserUsecaseImpl.kt
@@ -2,10 +2,12 @@ package ktorApiExample.core.application.usecases.addUserUsecase
 
 import ktorApiExample.core.domain.entities.User
 import ktorApiExample.core.domain.repositories.UsersRepository
+import java.util.*
 
 class AddUserUsecaseImpl(private val usersRepository: UsersRepository): AddUserUsecase {
     override fun invoke(request: AddUserUsecaseRequest): AddUserUsecaseResponse {
-        val user = User("userId1", request.name, request.email)
+        val newUserId = UUID.randomUUID().toString()
+        val user = User(newUserId, request.name, request.email)
         usersRepository.add(user)
         return AddUserUsecaseResponse(data=user)
     }
